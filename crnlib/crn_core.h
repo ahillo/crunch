d441ec254e917ed3c9e49d301411794e32218f69
Kostas Karanikolas <karanikolas@gmail.com>
2014-10-31 19:56:55 +0200

    Fixed CRNLIB_FORCE_INLINE having no effect on Clang

--- crnlib/crn_core.h
+++ crnlib/crn_core.h
@@ -83,7 +83,11 @@
 
    #define CRNLIB_RESTRICT
 
-   #define CRNLIB_FORCE_INLINE inline __attribute__((__always_inline__,__gnu_inline__))
+   #if defined(__clang__)
+      #define CRNLIB_FORCE_INLINE inline __attribute__((__always_inline__))
+   #else
+      #define CRNLIB_FORCE_INLINE inline __attribute__((__always_inline__,__gnu_inline__))
+   #endif
 
    #define CRNLIB_INT64_FORMAT_SPECIFIER "%lli"
    #define CRNLIB_UINT64_FORMAT_SPECIFIER "%llu"
